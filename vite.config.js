7da7e51c3cd85173133e8a7c49212705f8d229f3
Space Cadet 💻 <154667827+backendfrontflip@users.noreply.github.com>
2025-06-24 12:41:40 +0000

    all folders

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/leebyunghunent/', // 👈 Match your GitHub repo name
+  base: '/leebyunghunent/', 
   plugins: [react()],
 });
